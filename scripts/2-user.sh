dca8a3ab0109096502223c9b0489a993484dba31
Azraf Al Monzim <70007824+Monzim@users.noreply.github.com>
2022-03-08 14:18:55 +0600

    OneMore Test

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -70,6 +70,63 @@ if [[ $INSTALL_TYPE == "FULL" ]]; then
   fi
 fi
 
+bspwmConfig(){
+    cd ~
+    mkdir .config
+    git clone -b test https://github.com/Monzim/dotfiles
+    cd dotfiles
+    stow bspwm
+    stow sxhkd
+    stow zsh
+    stow kitty
+    stow polybar
+    stow zsh
+    cd ~
+}
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing BSPWM
+                     Made By MONZIM
+-------------------------------------------------------------------------
+"
+
+cd ~
+sed -n '/'$INSTALL_TYPE'/q;p' ~/ArchTitus/custom/pkg/pacman-pkgs.txt | while read line
+do
+  echo "INSTALLING: ${line}"
+  sudo pacman -S --noconfirm --needed ${line}
+done
+
+
+echo -ne "
+-------------------------------------------------------------------------
+Installing AUR Softwares
+-------------------------------------------------------------------------
+"
+addAUR () {
+  cd ~
+  git clone "https://aur.archlinux.org/$1.git"
+  cd ~/$1
+  makepkg -si --noconfirm
+  cd ~
+}
+
+if [${AURHELPER} -ne ""] then
+  addAUR ${AURHELPER}
+fi
+
+for PACKAGE in $(cat ~/ArchTitus/custom/pkg/aur-pkgs.txt) do
+  addAUR $PACKAGE
+esac
+
+echo -ne "
+-------------------------------------------------------------------------
+Configuring BSPWM
+-------------------------------------------------------------------------
+"
+bspwmConfig
+systemctl enable lightdm.service
+
 echo -ne "
 -------------------------------------------------------------------------
                     SYSTEM READY FOR 3-post-setup.sh
